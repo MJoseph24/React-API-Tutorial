fa66508ecb59a7009d1d1ff243e608736cf57398
Melvin <113693551+MJoseph24@users.noreply.github.com>
2022-11-29 12:27:49 -0500

    finished tutorial

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React JSON</h2>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
